abe40375cbd5b740f4dddedd65c0ddaa8573d759
imvictorwoo <baseballvictorr@gmail.com>
2019-07-26 13:06:19 -0500

    v1
    Utilizing Twitter for Sentiment Analysis

---
+++ Twitter-Sentiment/demo.py
@@ -0,0 +1,46 @@
+import tweepy
+from textblob import TextBlob
+import csv
+
+# Step 1 - Authenticate
+consumer_key= 'JYcaS7LrGZMB9w6z4MvD5JiAa'
+consumer_secret= 'ljyGM2ItUtPUTREWNZiMl7GJjLv84pBv7tx59jMzDRdrWzcbjG'
+
+access_token= '2162673698-5dMo4aqZhwrvWDBwjhg3TrQW5QDfWZarbX5WnQk'
+access_token_secret= 'DueMI2WMuK9Gy32T1Mqcedy8r5ysxstfkPCIgaIIhVAcm'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 3 - Retrieve Tweets
+name = 'Trump'
+public_tweets = api.search(name)
+
+def getSentiment(analysis):
+    if analysis.sentiment.polarity > 0:
+        return 'Positive'
+    else:
+        return 'Negative'
+        
+#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+#and label each one as either 'positive' or 'negative', depending on the sentiment 
+#You can decide the sentiment polarity threshold yourself
+
+# with open(name+'.csv', 'w') as csvFile:
+#     writer = csv.writer(csvFile)
+#     writer.writerows(csvData)
+# csvFile.close()
+
+for tweet in public_tweets:
+    if 'RT @' not in tweet.text:
+        print("Screen Name: ", tweet.user.screen_name)
+        print("Text: ", tweet.text,"\n\n")
+        analysis = TextBlob(tweet.text)
+        print("Analysis: ", getSentiment(analysis))
+    
+    #Step 4 Perform Sentiment Analysis on Tweets
+    # analysis = TextBlob(tweet.text)
+    # print(analysis.sentiment)
+    # print("")
